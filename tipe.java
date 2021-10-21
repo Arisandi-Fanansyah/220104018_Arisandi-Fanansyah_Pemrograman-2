acf076ca834c3d632f8dcc36b5f3a49b7a1ea1c8
Arisandi Fanansyah <92513119+Arisandi-Fanansyah@users.noreply.github.com>
2021-10-21 18:54:30 +0700

    Add files via upload

---
+++ tipe.java
@@ -0,0 +1,22 @@
+package percobaan;
+
+public class tipe {
+    public static void main(String[] args) {
+        long data1 = 756716542;
+        int data2 = 2234565;
+        short data3 = 716;
+        byte data4 = 75;
+        double data5 = 7.678;
+        float data6 = (float) 2.678;
+        char data7 = 'c';
+        boolean data8 = true;
+        System.out.println("Nilai long : " + data1);
+        System.out.println("Nilai int : " + data2);
+        System.out.println("Nilai short : " + data3);
+        System.out.println("Nilai byte : " + data4);
+        System.out.println("Nilai double : " + data5);
+        System.out.println("Nilai float : " + data6);
+        System.out.println("Nilai char : " + data7);
+        System.out.println("Nilai boolean : " + data8);
+    }
+}
